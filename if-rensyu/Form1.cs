23851b47c854bdede94f2ec9ae3373736d61465a
hukuma <pg175b046@gmail.com>
2017-06-02 14:53:10 +0900

    プロジェクトの登録

---
+++ if-rensyu/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_rensyu
+{
+    public partial class Form1 : Form
+    {
+        int vx = -20;
+        int vy = -20;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Left += vx;
+            button1.Top += vy;
+            if (button1.Left < 0 )
+            {
+                vx = 20;
+                vy = 20;
+            }
+            
+            if (button1.Top < 0){
+                vx = 20;
+                vy = -20;
+            }
+            
+            if (button1.Left > 500)
+            {
+                vx = 20;
+                vy = 20;
+            }
+            
+            if (button1.Top >= 500)
+            {
+                vx = -20;
+                vy = -20;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx -= 10;
+        }
+
+        
+    }
+}
